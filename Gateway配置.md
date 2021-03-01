9c2382103018ae4449a35b62b4e79e99abb8366d
蒙大拿 <mengqizhang@huluwa.cc>
2021-03-01 16:48:31 +0800

    feign负载调用服务，一个服务两个实体节点

--- Gateway配置.md
+++ Gateway配置.md
@@ -9,4 +9,20 @@
 - 配置Nacos配置中心
 - 配置Gateway全局过滤器、Routes路由转发过滤配置
 - Maven多环境配置+Nacos多环境命名空间
+
+
+
+#Gateway 路由谓词工厂：
+
+- 示例：
+
+spring:
+    cloud:
+        gateway:
+            routes:
+                - id: nacos-consumer-a #服务的id，可以自定
+                  uri: lb://nacos-consumer-a #负载的服务名，服务项目中的 spring.application.name
+                  predicates:
+                    - Path:/nacos-consumer-a/**,a/**  #Path是路由谓词，  
+
  
--- src/main/java/com/mqz/nacos/gateway/config/filter/GatewayGlobalFilter.java
+++ src/main/java/com/mqz/nacos/gateway/config/filter/GatewayGlobalFilter.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -47,17 +49,11 @@ public class GatewayGlobalFilter implements GlobalFilter,Ordered{
 
         System.out.println("网关读取配置文件中的变量："+about);
 
+
+
         ServerHttpRequest serverHttpRequest = exchange.getRequest();
         URI uri = serverHttpRequest.getURI();
         System.out.println(uri.getPath());
-        //根据请求路径拦截、放行指定的请求
-        String path = uri.getPath();
-        if(needPermission.contains(path)){
-            String tokenValue = serverHttpRequest.getQueryParams().getFirst("token");
-            if(StringUtils.isEmpty(tokenValue)){
-                throw new WithoutLoginException();
-            }
-        }
 
         //URL请求参数
         MultiValueMap<String,String> map = serverHttpRequest.getQueryParams();
@@ -71,7 +67,50 @@ public class GatewayGlobalFilter implements GlobalFilter,Ordered{
         }
         Flux<DataBuffer> body = serverHttpRequest.getBody();
         System.out.println(body);
-        return chain.filter(exchange);
+
+        //根据请求路径拦截、放行指定的请求
+        String path = uri.getPath();
+        if(needPermission.contains(path)){
+            String tokenValue = serverHttpRequest.getQueryParams().getFirst("token");
+            if(StringUtils.isEmpty(tokenValue)){
+                throw new WithoutLoginException();
+            }
+        }
+
+        String tokenValue = serverHttpRequest.getQueryParams().getFirst("token");
+        if(StringUtils.isEmpty(tokenValue)){
+            throw new WithoutLoginException("请携带登录的token");
+        }
+        //模拟解析token
+        String userId = tokenValue.split("zzz")[1];
+
+
+        StringBuilder query = new StringBuilder();
+        //获取请求uri的请求参数（GET请求参数通过拼接key=value形式进行传参）
+        String originalQuery = uri.getRawQuery();
+
+        //判断最后一个字符是否是&，如果不是则拼接一个&，以备后续的参数进行连接
+        if (StringUtils.hasText(originalQuery)) {
+            query.append(originalQuery);
+            if (originalQuery.charAt(originalQuery.length() - 1) != '&') {
+                query.append('&');
+            }
+        }
+        //获取config中的key、value，然后拼接到uri请求参数后面
+        // TODO urlencode?
+        query.append("GATEWAY_USER_ID");
+        query.append('=');
+        query.append(userId);
+
+        //把请求参数重新拼接回去，并放入request中传递到过滤链的下一个请求中去
+        try {
+            URI newUri = UriComponentsBuilder.fromUri(uri).replaceQuery(query.toString()).build(true).toUri();
+            ServerHttpRequest request = exchange.getRequest().mutate().uri(newUri).build();
+            return chain.filter(exchange.mutate().request(request).build());
+        }catch (RuntimeException ex) {
+            throw new IllegalStateException("Invalid URI query: " + query.toString());
+        }
+//        return chain.filter(exchange);
     }
 
 
